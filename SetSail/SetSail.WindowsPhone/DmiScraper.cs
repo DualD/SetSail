0ade0c33a31b2ed4b68de6499341133660953076
Danny Nielsen <danny.nlsn0@gmail.com>
2015-08-03 17:13:11 +0200

    Startup

---
+++ SetSail/SetSail.WindowsPhone/DmiScraper.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using HtmlAgilityPack;
+
+namespace SetSail
+{
+    class DmiScraper
+    {
+        private HtmlWeb _webPage = new HtmlWeb();
+        private HtmlDocument _webPageDoc;
+
+        public async void LoadUrl(string url)
+        {
+            _webPageDoc = new HtmlDocument();
+        }
+        
+        public void PullText(string xPath)
+        {
+            HtmlWeb web = new HtmlWeb();
+            
+            
+           
+        }
+    }
+}
